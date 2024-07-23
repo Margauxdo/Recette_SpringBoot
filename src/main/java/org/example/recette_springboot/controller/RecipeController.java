f96b8e1dfc75c547ec3d26e594c3e6982e1ec10c
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-23 16:59:12 +0200

    add recipe controller

--- src/main/java/org/example/recette_springboot/controller/RecipeController.java
+++ src/main/java/org/example/recette_springboot/controller/RecipeController.java
@@ -1,11 +1,15 @@
 package org.example.recette_springboot.controller;
 
 
+import jakarta.validation.Valid;
 import org.example.recette_springboot.model.Recipe;
 import org.example.recette_springboot.service.IRecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,6 +28,21 @@ public class RecipeController {
         List<Recipe> recipes = recipeService.getALLRecipes();
         model.addAttribute("recipes", recipes);
         return "recipes";
+    }
+    @GetMapping("/add")
+    public String addRecipe(Model model) {
+        model.addAttribute("recipe", new Recipe());
+        return "formRecipe";
+    }
+    @PostMapping("/add")
+    public String addRecipe(@Valid  @ModelAttribute("recipe") Recipe recipe, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "formRecipe";
+        }else {
+            recipeService.saveRecipe(recipe);
+            return "redirect:/recipe/list";
+        }
     }
 
 }
