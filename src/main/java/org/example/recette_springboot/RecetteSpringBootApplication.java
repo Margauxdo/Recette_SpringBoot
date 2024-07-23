7419aa5e79b6abb40cf934226d15fab09461c679
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-23 12:29:26 +0200

    Initial commit

---
+++ src/main/java/org/example/recette_springboot/RecetteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.recette_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetteSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetteSpringBootApplication.class, args);
+	}
+
+}
